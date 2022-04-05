e1a6da2799d88a15c067e0c23527d69d14037e77
Kir1K2313 <100360486+Kir1K2313@users.noreply.github.com>
2022-04-05 18:53:03 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -32,6 +32,9 @@ int main()
 {
     txCreateWindow (1200, 900);
 
+    txDisableAutoPause();
+
+    //Загрузка картинок
     HDC fon = txLoadImage("Картинки/фон/фон.bmp");
 
     HDC right = txLoadImage("Картинки/машина/right.bmp");
@@ -40,15 +43,15 @@ int main()
 
     HDC Ship = txLoadImage("Картинки/препятствие/Шипы.bmp");
 
+    HDC FON = txLoadImage("Картинки/главное меню/фон.bmp");
 
    char score_list[100];
    int score = 0;
    int level = 1;
-    txSetColor(TX_WHITE);
-    txSetFillColor (TX_BLACK);
 
+    //машина
     Personage car = {50, 750, right, true};
-
+    //Монета
     int kolmoney = 50;
     Personage money[kolmoney];
     for(int i=0; i<kolmoney; i++)
@@ -83,10 +86,16 @@ int main()
     {
         txClear();
 
+        //Главное меню
 
         if(PAGE=="Меню")
         {
-            txSetFillColor (TX_BLACK);
+
+            txSetColor(TX_WHITE,5);
+            txSetFillColor (TX_RED);
+
+            txTransparentBlt (txDC(), 0, 0, 1200,900, FON, 0, 0,TX_GREEN);
+
             txRectangle(500,100,700,150);
             txDrawText(500,100,700,150,"START");
 
@@ -98,16 +107,54 @@ int main()
             }
 
                txRectangle(500,200,700,250);
-               txDrawText(500,200,700,250,"EXIT");
+               txDrawText(500,200,700,250,"HELP");
 
-            if(txMouseX() >= 500 && txMouseY()>= 200 &&
-               txMouseX() <= 700 && txMouseY()<= 250 &&
+           if(txMouseX() >= 500 && txMouseY()>= 200 &&
+           txMouseX() <= 700 && txMouseY()<= 250 &&
+           txMouseButtons() == 1)
+           {
+                PAGE="Справка";
+           }
+
+                txRectangle(500,300,700,350);
+                txDrawText(500,300,700,350,"EXIT");
+
+            if(txMouseX() >= 500 && txMouseY()>= 300 &&
+               txMouseX() <= 700 && txMouseY()<= 350 &&
                txMouseButtons() == 1)
             {
-               return 0;
+                return 0;
             }
+
+
         }
 
+       if(PAGE=="Справка")
+       {
+         txRectangle(100,100,300,150);
+         txDrawText(100,100,300,150, "Назад");
+            if(txMouseX() >= 100 && txMouseY()>= 100 &&
+            txMouseX() <= 300 && txMouseY()<= 150 &&
+            txMouseButtons() == 1)
+            {
+                 PAGE= "Меню";
+            }
+
+         txDrawText(400,100,800,600,"Управление - стрелками\n"
+                                     "\n"
+                                     "монеты - прибавляют очки\n"
+                                     "\n"
+                                     "шипы - убивают тебя\n"
+                                     "\n"
+                                     "Цель игры - собрать 30 монет\n"
+                                     "\n"
+                                     "Удачной игры =)");
+
+       }
+
+
+
+        //ИГРА
         if(PAGE =="Игра")
         {
 
@@ -196,8 +243,9 @@ int main()
             if(car.visible == false)
             {
                 txSetFillColor(TX_RED);
-                txRectangle(500,100,1000,400);
-                txDrawText(500,100,1000,150,"Game over");
+                txRectangle(500,100,700,200);
+                txDrawText(500,100,700,200,"Game over");
+
             }
 
          for(int i=0; i<kolship; i++)
@@ -207,7 +255,7 @@ int main()
               road2[0].x = road2[0].x - 15;
               road3[0].x = road3[0].x - 15;
               ship[i].x = ship[i].x - 15;
-              money[i].x = ship[i].x - 15;
+              money[i].x = money[i].x - 15;
 
             }
 
@@ -250,6 +298,7 @@ int main()
         txDeleteDC(mon);
         txDeleteDC(fon);
         txDeleteDC(Ship);
+        txDeleteDC(FON);
 
 return 0;
 
